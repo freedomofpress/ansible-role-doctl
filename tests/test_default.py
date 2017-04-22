9765ce2635cbc51e9e93b3932de5333a716e22cf
Conor Schaefer <conor@freedom.press>
2017-04-22 16:41:00 -0700

    Updates config test suite
    Tests that the command `doctl` actually exists.

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,12 +4,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
-def test_
+def test_doctl_command_exits(Command):
+    assert Command.exists("doctl")

f389cb7086351824c429e8f1588d509514012f30
Conor Schaefer <conor@freedom.press>
2017-04-22 16:43:23 -0700

    Adds version check to config tests
    Will require manual updating every time the role default var changes,
    but that's what we want.

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,5 +4,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
+DOCTL_VERSION = "1.6.0"
+
+
 def test_doctl_command_exits(Command):
     assert Command.exists("doctl")
+
+
+def test_doctl_version(Command):
+    version_string = "doctl version {}-release".format(DOCTL_VERSION)
+    c = Command("doctl version")
+    assert c.rc == 0
+    assert version_string in c.stdout
